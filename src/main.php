bb87a5485107c8ee5930a3b10ad120a7761d5fd8
TeMp3r126 <42579059+TeMp3r126@users.noreply.github.com>
2018-09-13 20:51:53 -0400

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,1 +1,36 @@
+<?php
 
+namespace Heal;
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+class Main extends PluginBase{
+
+    public function onEnable("Heal Enabled"){
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+
+        if($cmd->getName() == "heal"){
+
+                if($sender instanceof Player){
+			if($sender->hasPermission("heal.command")){
+				$sender->setHealth(20);
+				$sender->sendMessage("You have been healed!");
+			}
+			   
+                }
+            break;
+
+        }
+
+        return true;
+
+    }
+
+}
